1458a6eaeb602fe22c25c2fda1a985dd40730072
wooyeoungAhn <wyahn1219@gmail.com>
2024-05-08 12:25:15 +0900

    commit

--- SKHynix/scripts/FileLifeCycleManager/FLM.py
+++ SKHynix/scripts/FileLifeCycleManager/FLM.py
@@ -7,6 +7,7 @@ import pandas
 import json
 import logging
 import traceback
+import yaml
 parser = argparse.ArgumentParser(description="Please refer to Netapp korea github : https://github.com/netappkr/AnsiblePlaybook/tree/main/SKHynics/scripts")
 parser.add_argument("-f", "--file", type=str, nargs='+', help="read filenames example: -f filename1 filename2", required=False)
 parser.add_argument("-r", "--request", type=str, help="request type",required=False)
@@ -24,3 +25,32 @@ formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s',d
 file_handler = logging.FileHandler('flm.log', mode='a') ## 파일 핸들러 생성
 file_handler.setFormatter(formatter) ## 텍스트 포맷 설정
 logger.addHandler(file_handler) ## 핸들러 등록
+
+def check_yaml_integrity(file_path):
+    required_structure = {
+        'exportpolicy': {
+            'name': str
+        },
+        'kind': {
+            'division': str
+        }
+    }
+    # YAML 파일 로드
+    try:
+        with open(file_path, 'r') as file:
+            config = yaml.safe_load(file)
+    except Exception as e:
+        return f"Error reading YAML file: {e}"
+
+
+
+def main():
+    # cURL command's target URL
+    # url = 'http://10.10.242.101:12993/metrics'  # Replace with your actual URL
+    # 무결성 검사 실행
+    print(check_yaml_integrity(args.config))
+    # test.
+    # read_the_file_and_save_metrics(files,args.output_json_file_path,includelist,excludelist)
+
+if __name__ == "__main__":
+    main()
