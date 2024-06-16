11250270f56eeb97f5a2836d296f4c885a44711e
wooyoeungAhn <wyahn1219@gmail.com>
2024-06-17 01:28:15 +0900

    commit

--- SKHynix/scripts/FileLifeCycleManager/AutoPath/Autopath.py
+++ SKHynix/scripts/FileLifeCycleManager/AutoPath/Autopath.py
@@ -33,7 +33,7 @@ def read_yaml_config(config_file_path):
     with open(config_file_path, 'r', encoding='utf-8') as config_file:
         return yaml.safe_load(config_file)
 
-def main(data_file_path, auto_sim_file_path, config_file_path, searchdirs):
+def main(data_file_path, auto_sim_file_path, searchdirs):
     replacement_dict = read_auto_sim(auto_sim_file_path)
     data_lines = read_data_file(data_file_path)
     modified_lines = modify_lines(data_lines, replacement_dict)
@@ -55,10 +55,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Please refer to Netapp korea github : https://github.com/netappkr/AnsiblePlaybook/tree/main/SKHynics/scripts")
     parser.add_argument("-f", "--file", type=str, required=True, help="Path to the data file")
     parser.add_argument("-a", "--auto", type=str, required=True, help="Path to the auto.sim file")
-    parser.add_argument("--config", type=str, required=False, help="Path to the config YAML file")
+    # parser.add_argument("--config", type=str, required=False, help="Path to the config YAML file")
     parser.add_argument("--searchdir", type=str, nargs='+', required=True, help="List of search directories")
     args = parser.parse_args()
 
-    main(args.file, args.auto, args.config, args.searchdir)
+    # main(args.file, args.auto, args.config, args.searchdir)
+    main(args.file, args.auto, args.searchdir)
 
     
