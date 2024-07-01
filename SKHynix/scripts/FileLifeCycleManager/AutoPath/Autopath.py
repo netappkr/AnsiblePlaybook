c65f10ac86ae050d8aecb1bc49912c0c087c183c
wooyeoungAhn <wyahn1219@gmail.com>
2024-07-01 13:48:59 +0900

    commit

--- SKHynix/scripts/FileLifeCycleManager/AutoPath/Autopath.py
+++ SKHynix/scripts/FileLifeCycleManager/AutoPath/Autopath.py
@@ -49,7 +49,7 @@ def result_data(jobstatus,message,xcpinfo,xcpresult,replace):
         "xcp_result": xcpresult,
         "replace": replace
     }
-    return json.dump(result)
+    return json.dump(result,ensure_ascii=False)
 
 def main(xcpresult, xcpinfo, replace, automap, searchdirs, volumename, status, skipdedup):
     try:
